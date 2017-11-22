1b6917da4a672f5c3d91ec48940ce1cc0304bc90
Josh Cross <crossjoshua6@gmail.com>
2017-11-22 12:00:41 +0000

    Added a simple GUI that displays the names of all the games that are stored in the DB

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Data.SqlClient;
+using System.Drawing;
+
+
+namespace TheGameShop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+
+
+
+        
+        
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+
+            connection.Close();
+
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gameForm = new System.Windows.Forms.ListBox();
+            this.game = new System.Windows.Forms.Label();
+            this.description = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // gameForm
+            // 
+            this.gameForm.FormattingEnabled = true;
+            this.gameForm.Location = new System.Drawing.Point(154, 10);
+            this.gameForm.Name = "gameForm";
+            this.gameForm.Size = new System.Drawing.Size(364, 615);
+            this.gameForm.TabIndex = 0;
+            this.gameForm.SelectedIndexChanged += new System.EventHandler(this.gameForm_SelectedIndexChanged);
+            // 
+            // game
+            // 
+            this.game.AutoSize = true;
+            this.game.BackColor = System.Drawing.Color.Transparent;
+            this.game.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.game.Location = new System.Drawing.Point(542, 10);
+            this.game.Name = "game";
+            this.game.Size = new System.Drawing.Size(145, 55);
+            this.game.TabIndex = 1;
+            this.game.Text = "game";
+            // 
+            // description
+            // 
+            this.description.Location = new System.Drawing.Point(557, 84);
+            this.description.Name = "description";
+            this.description.Size = new System.Drawing.Size(511, 328);
+            this.description.TabIndex = 2;
+            this.description.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1395, 637);
+            this.Controls.Add(this.description);
+            this.Controls.Add(this.game);
+            this.Controls.Add(this.gameForm);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox gameForm;
+        private Label game;
+        private ListView description;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Data.SqlClient;
+using System.Drawing;
+
+namespace TheGameShop
+{
+    public partial class Form1 : Form
+    {
+
+        SqlConnection connection;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        //when the form is first created.
+        protected override void OnCreateControl()
+        {
+            base.OnCreateControl();
+
+            //the background color of the website.
+            Color bkg = Color.FromArgb(25, 25, 25);
+            //the color of elements such as gameForm.
+            Color elementColor = Color.FromArgb(17, 17, 17);
+            //the text colour on the application
+            Color textColor = Color.FromArgb(255, 255, 255);
+
+            //setting the background colour for the application.
+            BackColor = bkg;
+
+            gameForm.BackColor = elementColor;
+            gameForm.ForeColor = textColor;
+            description.BackColor = elementColor;
+            description.ForeColor = elementColor;
+
+            game.ForeColor = textColor;
+
+            dbConnect();
+
+        }
+
+        //this is fired when something is selected from an item is selected from ListBox.
+        private void gameForm_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            game.Text = gameForm.SelectedItem.ToString();
+            msg("Yes!");
+
+            //try to add to the description
+            try
+            {
+                //getting the title of games from the Games table.
+                SqlCommand command = new SqlCommand("SELECT * FROM Games", connection);
+
+
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                //reading from the database.
+                while (reader.Read())
+                {
+                    //Getting the ID column.
+                    String ID = reader["ID"].ToString();
+                    //Printing the ID.
+                    Console.WriteLine("ID: " + ID);
+
+                    
+
+                    //Getting the platform column
+                    String platform = reader["Platform"].ToString();
+                    Console.WriteLine("Platform: " + platform);
+                    description.Items.Add(platform);
+
+                    //Getting the Genre of the game
+                    String genre = reader["Genre"].ToString();
+                    Console.WriteLine("Genre: " + genre);
+
+                    //printing the age rating of the game.
+                    String age = reader["AgeRating"].ToString();
+                    Console.WriteLine("Age rating: " + age + "\n\n");
+
+
+                }
+            } catch
+            {
+                
+            }
+        }
+
+        private void msg(String message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void dbConnect()
+        {
+            String connectionString = "Data Source=DESKTOP-MA119SL\\SQLEXPRESS;Initial Catalog=GameDatabase; Integrated Security=True";
+
+            bool hasConnected = false;
+            //Connecting to the database that was created in SQL Management Studio.
+            connection = new SqlConnection(connectionString);
+
+            connection.Open();
+
+            System.Threading.Thread.Sleep(1000);
+
+
+
+            try
+            {
+
+                Console.WriteLine("we're in \n");
+
+                //getting the title of games from the Games table.
+                SqlCommand command = new SqlCommand("SELECT * FROM Games", connection);
+
+
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                //reading from the database.
+                while (reader.Read())
+                {
+                    //Getting the ID column.
+                    String ID = reader["ID"].ToString();
+                    //Printing the ID.
+                    Console.WriteLine("ID: " + ID);
+
+                    //Getting the games column
+                    String games = reader["Games"].ToString();
+                    gameForm.Items.Add(games);
+                    //Getting the platform column
+                    String platform = reader["Platform"].ToString();
+                    Console.WriteLine("Platform: " + platform);
+
+                    //Getting the Genre of the game
+                    String genre = reader["Genre"].ToString();
+                    Console.WriteLine("Genre: " + genre);
+
+                    //printing the age rating of the game.
+                    String age = reader["AgeRating"].ToString();
+                    Console.WriteLine("Age rating: " + age + "\n\n");
+
+
+                }
+
+
+                reader.Close();
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            if (Debugger.IsAttached)
+            {
+                Console.ReadLine();
+            }
+
+            gameForm.Items.Add("Hello");
+        }
+
+        //function that disconnects with the server
+        public void dbDisconnect()
+        {
+            connection.Close();
+        }
+
+
+    }
+}
